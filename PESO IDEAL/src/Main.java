288f8518490a44c7265d7efc3e67e46db629848b
huugoo310 <hugomartinez310707@gmail.com>
2025-10-22 09:35:06 +0200

    commit

---
+++ PESO IDEAL/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String Enunciado = "CALCULADOR DE PESO IDEAL";
+        System.out.println(Enunciado);
+
+        System.out.println("Indica tu Altura(cm):");
+        int altura = sc.nextInt();
+        System.out.println("Tu Altura" + " " + altura);
+
+        System.out.println("Indica tu Peso(kg):");
+        int peso = sc.nextInt();
+        System.out.println("Tu Peso" + " " + peso);
+
+        int total = altura - 100;
+
+        System.out.println("Tu peso ideal es de " + " " + total + "kg44");
+
+
+    }
+}
